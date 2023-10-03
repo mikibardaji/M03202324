211b602a5b56c8ed935e79289cbc617399ad9f3d
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2023-10-03 12:56:36 +0200

    salarioimpuestos

--- UF1-NF1(IF-BUCLES)/Exercici18Salario/src/exercici18salario/Exercici18Salario.java
+++ UF1-NF1(IF-BUCLES)/Exercici18Salario/src/exercici18salario/Exercici18Salario.java
@@ -17,14 +17,14 @@ public class Exercici18Salario {
      */
     public static void main(String[] args) {
         int horas, h_extra; //Es enter perque no tractem amb mitjes hores
-        int HORAS_NORMAL = 35; //es enter perque no te sentit que sigui hores partides
-        double T_NORMAL = 20; //el fico double, perque et podrien pagar la hora a 10.5
-        double PRECIOS_EXTRA = 1.5;
+        final int HORAS_NORMAL = 40; //es enter perque no te sentit que sigui hores partides
+        final double T_NORMAL = 20; //el fico double, perque et podrien pagar la hora a 10.5
+        final double PRECIOS_EXTRA = 1.5;
         double salario, salario_extra, salario_normal; //
         Scanner sc = new Scanner(System.in);
         System.out.print("Quantes hores has treballat?...");
         horas = sc.nextInt();
-        
+
         if (horas>HORAS_NORMAL)
         { //tinc que pagar hores extres
             h_extra = horas - HORAS_NORMAL;
@@ -32,17 +32,44 @@ public class Exercici18Salario {
             salario_extra = h_extra*T_NORMAL*PRECIOS_EXTRA;
             salario_normal = HORAS_NORMAL*T_NORMAL;
             salario = salario_extra + salario_normal;
-            System.out.println("Tu salario es... " + salario);
         }
         else
         { //tarifa normal
             //no definicio de variables
             salario = horas * T_NORMAL;
-            System.out.println("Tu salario es... " + salario);
         }
         
-        
-        
+        System.out.println("Tu salario BRUTO es... " + salario);
+        final int SALARIO_SIN_IMPUESTOS = 500;
+        final int SALARIO_TOPE_IMPUESTOS = 900;
+        //double impuestos;
+        double impuestos=0, impuestos_25;
+        //double salario_neto;
+        double salario_impuestos, salario_tope_impuestos;
+        //calcul de impostos
+        if(salario>SALARIO_TOPE_IMPUESTOS)
+        { // he superado el tope y tengo que aplicar 45%
+            salario_tope_impuestos = salario - SALARIO_TOPE_IMPUESTOS;
+            impuestos = salario_tope_impuestos*0.45; //impuesto de lo que se pasa del tope
+            salario_impuestos = SALARIO_TOPE_IMPUESTOS - SALARIO_SIN_IMPUESTOS;
+            impuestos_25 = salario_impuestos * 0.25;
+            impuestos = impuestos + impuestos_25 ;
+        }
+        else if (salario>SALARIO_SIN_IMPUESTOS &&
+                salario < SALARIO_TOPE_IMPUESTOS)
+        {//tengo que calcular los impuestos
+            salario_impuestos = salario - SALARIO_SIN_IMPUESTOS;
+            //System.out.println("salario a aplicar impuestos.... " + salario_impuestos);
+            impuestos = (salario_impuestos * 25)/100; // * 0.25;
+            System.out.println("         impuestos .... " + impuestos);
+        }
+        else // podria borrar el else porque no hace nada...
+        {//no hay impuestos
+          impuestos = 0;  
+        }
+        //salario_neto = salario - impuestos;
+        salario = salario - impuestos;
+        System.out.println("     Salario NETO .... " + salario);
         
     }
     
