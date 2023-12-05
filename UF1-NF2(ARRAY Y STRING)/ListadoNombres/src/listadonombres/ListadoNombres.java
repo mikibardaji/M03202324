36e43bb2c8ec3e036ed8f735b3e73f6e478f1a99
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2023-12-05 12:15:59 +0100

    agendainacabada

--- UF1-NF2(ARRAY Y STRING)/ListadoNombres/src/listadonombres/ListadoNombres.java
+++ UF1-NF2(ARRAY Y STRING)/ListadoNombres/src/listadonombres/ListadoNombres.java
@@ -14,21 +14,27 @@ public class ListadoNombres {
 
     public static void main(String[] args) {
         //definir estructura de string per 10 noms
+        final int CAPACIDAD_AGENDA = 10;
+        String[] agenda = new String[CAPACIDAD_AGENDA];
         char opcio;
-
+        Scanner sc = new Scanner(System.in);
+        cargarAgenda(agenda);
+        
         do {
+            System.out.println("Aprieta enter para mostrar menu");
+            sc.nextLine(); //para i al apretar el enter
             mostrarMenu();
             opcio = obtenirOpcioUsuari();
 
             switch (opcio) {
-                case 'a':
-                    //opcio
+                case 'a': //llistar
+                    llistarAgenda(agenda);
                     break;
-                case 'b':
-                    //opcio
+                case 'b': //Afegir nom
+                    afegirNomAgenda(agenda);
                     break;
                 case 'c':
-                    //opcio
+                    modificarNomAgenda(agenda);
                     break;
                 case 'd':
                     //opcio
@@ -38,6 +44,7 @@ public class ListadoNombres {
                     break;
                 case 'f':
                     //opcio
+                    //salir=true;
                     break;
                 case 'g':
                     //opcio
@@ -46,7 +53,8 @@ public class ListadoNombres {
                     System.out.println("Opció no v?lida. Torna a intentar.");
             }
 
-        } while (/*fica opció per continuar dins el bucle*/);
+        } while (opcio!='f');
+        System.out.println("finalizando aplicacion");
     }
 
     private static void mostrarMenu() {
@@ -65,7 +73,85 @@ public class ListadoNombres {
 
     System.out.print ("Selecciona una opció: ");
 //recollir lletra i retornar-la
-return scanner.next().charAt(0);
-}
+    return scanner.next().charAt(0);
+    }
+
+    private static void cargarAgenda(String[] agenda) {
+        agenda[0] = "Laura Montesinos";
+        agenda[1] = "Samuel Fuente";
+        agenda[2] = "Toni Martí";
+    }
+
+    
+    /**
+     * mostrar tots els noms de la agenda
+     * no ha de mostrar els null
+     * i al final de tot el llistat te que mostrar quants noms ha ensenyat
+     * @param agenda conjunt noms
+     */
+    private static void llistarAgenda(String[] agenda) {
+        int i,num_contactos=0;
+        for (i = 0; i < agenda.length; i++) {
+            if (agenda[i]!=null)
+            {
+                System.out.println((i+1)+ " - " + agenda[i] );
+                num_contactos++;
+            }
+            //if (agenda[i]==null) no hay mas
+            //{ 
+            //  i = agenda.length;
+            //} //surt del bucle
+        }
+        System.out.println("Numero de contactos mostrados " + num_contactos);
+    }
+
+    /**
+     * Demanara un nom per afegir a la agenda
+     * Buscar  la primera posició buida (null), i el  fica..
+     * Un cop afegida ha d'informar que ho ha fet al usuari.
+     * v2.o si han afegit un nom que ja existeix, que informi  i no ho afegeixi
+     * @param agenda 
+     */
+    private static void afegirNomAgenda(String[] agenda) 
+    {
+        boolean existe=false, afegit=false;
+        int index=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Posa el nom a afegir");
+        String nom_afegir = sc.nextLine();
+        while (!existe && !afegit && index<agenda.length) //afegit==false)
+        {
+            if(agenda[index]==null) //posició lliure
+            {
+                agenda[index] = nom_afegir;
+                afegit=true; //per sortir del bucle
+                System.out.println("Nom afegit...");
+            }
+            else if(agenda[index].equalsIgnoreCase(nom_afegir))
+            {
+                System.out.println("Nom ja existeix");
+                existe=true;
+            }
+            index++;
+        }
+        if(!afegit && !existe)
+        {
+            System.out.println("No hi ha espai");
+        }
+        
+    }
+
+    /**
+     * Demanar un nom a canviar
+     * Buscar si existeix
+     *    * Si l'ha trobat Demanar el nou nom i el canvia
+          * Si ho canvia avisar al usuari
+     * Si no ho canvia també ha de avisar
+     * si troba un null ja no existeix
+     * @param agenda 
+     */
+    private static void modificarNomAgenda(String[] agenda) {
+        
+    }
     
 }
