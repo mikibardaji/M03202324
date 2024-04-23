14ce431d782ed66f6403128f323e3195d3048b9a
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2024-04-23 12:59:40 +0200

    sql

--- uf6/ConexioBD/src/conexiobd/ConexioBD.java
+++ uf6/ConexioBD/src/conexiobd/ConexioBD.java
@@ -8,10 +8,15 @@ import BD.DBConnect;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.sql.ResultSet;
+//import java.sql.ResultSetMetaData;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Country;
 /**
  *
  * @author mabardaji
@@ -44,6 +49,12 @@ public class ConexioBD {
                     case 1:
                         printAllCountries();
                         break;
+                    case 2:
+                        printAllMetadataCountries();
+                        break;                     
+                    case 3:
+                        InsertCountry2();
+                        break;                         
                 }
  
                 
@@ -81,8 +92,9 @@ public class ConexioBD {
     private void printAllCountries() {
         try {
             Connection conn;
-            
+            int counter = 0;
             conn = DBConnect.getConnection();
+            Country show_country = new Country();
             if (conn!=null)
             {
                 //statement es el que tindra la instrucció sql
@@ -91,8 +103,7 @@ public class ConexioBD {
                 String query = "SELECT * from countries";
                 //Resultset similar a cursor SQL
                 ResultSet res = stmt.executeQuery(query);// Open Cursor
-                
-                
+           
                 //fetch
                 while(res.next())
                 {
@@ -100,8 +111,16 @@ public class ConexioBD {
                     String name = res.getString("name");
                     String capital = res.getString("capital");
                     System.out.println(id + "-" + name + "-" + capital);
-                    
+                    //show_country = new Country(id, name, capital, id, id, id, id);
+                    //al no recuperarlos tots, faig els setters del camps 
+                    show_country.setId(id);
+                    show_country.setName(name);
+                    show_country.setCapital(capital);
+                    System.out.println(show_country);
+                    counter++;
                 }
+                System.out.println("Paises mostrados....");
+                //mostrar quants paisos he ensenyat
                 res.close();
                 stmt.close();
                 
@@ -116,6 +135,117 @@ public class ConexioBD {
             System.out.println("Error haciendo conexion" + ex.getMessage());
         }
     }
+
+    /* imprimir metadatos del cursor */
+    private void printAllMetadataCountries() {
+         try {
+            Connection conn;
+            int counter = 0;
+       
+            conn = DBConnect.getConnection();
+            
+            //statement es el que tindra la instrucció sql
+            Statement stmt = conn.createStatement();
+                
+            String query = "SELECT * from countries";
+            //cursor igual
+            ResultSet cursor = stmt.executeQuery(query);
+            //vaig a averiguar els metadatos del cursor
+            ResultSetMetaData cursor_mtd = cursor.getMetaData();
+            int num_col = cursor_mtd.getColumnCount();
+            for (int i = 0; i < num_col; i++) {
+                System.out.print(cursor_mtd.getColumnLabel(i+1) + "\t");
+                System.out.print(cursor_mtd.getColumnName(i+1) + "\t");
+                System.out.print(cursor_mtd.getColumnType(i+1) + "\t");
+                System.out.print(cursor_mtd.getColumnTypeName(i+1) + "\t");
+                System.out.print(cursor_mtd.isAutoIncrement(i+1) + "\t");
+                System.out.print(cursor_mtd.isNullable(i+1) + "\t");
+                System.out.print(cursor_mtd.getPrecision(i+1) + "\t");
+                System.out.println("");
+            }
+            
+            
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexioBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /*
+    inserta un pais fixe, fica't per codi
+    */
+    private void InsertCountry() {
+        try {
+            Connection conn;
+            
+            conn = DBConnect.getConnection();
+            if (conn != null)
+            {
+                //statement es el que tindra la instrucció sql
+                Statement stmt = conn.createStatement();
+                //Creem query de insert
+                String query = "INSERT INTO COUNTRIES  VALUES"
+                        + " (NULL,'El salvador','El salvador',100,50,2.0,83)";
+                //verificacio
+                System.out.println(query);
+                
+                int numRowsAffected = stmt.executeUpdate(query);
+                System.out.println("Pais insertado");
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexioBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /*
+    inserta un pais fixe, fica't per codi
+    */
+    private void InsertCountry2() {
+        try {
+            Connection conn;
+            
+            conn = DBConnect.getConnection();
+            if (conn != null)
+            {
+                
+                //Creem query de insert
+                String query = "INSERT INTO COUNTRIES  "
+                        + "(name,capital,surface,inhabitants,pib,lifeexpectancy) "
+                        + " VALUES"
+                        + " (?,?,?,?,?,?)";
+                //verificacio
+                System.out.println(query);
+               
+                PreparedStatement preparedQuery = conn.prepareStatement(query);
+                //informo cada interrogant amb el seu valor
+                preparedQuery.setString(1, "El salvador2");
+                preparedQuery.setString(2, "Capital");
+                preparedQuery.setDouble(3,23.00);
+                preparedQuery.setInt(4, 1000);
+                preparedQuery.setDouble(5,123.00);
+                preparedQuery.setInt(6,23);
+                
+                int rows = preparedQuery.executeUpdate();
+                System.out.println("Pais insertado... " + rows);
+//                if (!preparedQuery.executeUpdate())
+//                {
+//                    System.out.println("no ejecutado correctamente");
+//                }
+//                else
+//                {
+//                    System.out.println("Insertado correctamente");
+//                }
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println(ex.getSQLState());
+        }
+    }
+    
+    
     
     
 }
