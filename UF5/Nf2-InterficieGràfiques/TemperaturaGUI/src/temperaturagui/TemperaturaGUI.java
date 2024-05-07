22c8bd331210b5b5e5db9ce6420fe1592f42cf27
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2024-05-07 12:57:27 +0200

    uf5nf2 primer dia

--- UF5/Nf2-InterficieGràfiques/TemperaturaGUI/src/temperaturagui/TemperaturaGUI.java
+++ UF5/Nf2-InterficieGràfiques/TemperaturaGUI/src/temperaturagui/TemperaturaGUI.java
@@ -4,6 +4,14 @@
  */
 package temperaturagui;
 
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
 /**
  *
  * @author mabardaji
@@ -27,6 +35,35 @@ public class TemperaturaGUI {
         //etiqueta temperatura - Radiobutton Celsius
         //cuadre text          - RadioButton Farenheit
         //boto convertir       - RadioButton Kelvin
+         JFrame window = new JFrame("Temperatura ");
+         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+         
+         Container pane = window.getContentPane();
+        //disposició dels components dintre aquest panel contenidor
+        pane.setLayout(new GridLayout(3, 2));
+        
+        
+        JLabel etiqueta = new JLabel("Pon temperatura");
+        JRadioButton radio1 = new JRadioButton("Celsius");
+        JRadioButton radio2 = new JRadioButton("Farenheit");
+        JRadioButton radio3 = new JRadioButton("Kelvin");
+        JButton calcular = new JButton("Convertir");
+        JTextField entrada = new JTextField();
+        
+        
+        pane.add(etiqueta);
+        pane.add(radio1);
+        pane.add(entrada);
+        pane.add(radio2);
+        pane.add(calcular);
+        pane.add(radio3);
+        
+        window.setSize(200, 200);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setResizable(false);
+        
     }
     
 }
